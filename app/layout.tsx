edbb14bd897821cae000b2101a04c9dff74c1836
AnvarMEDIA <35332812+AnvarMEDIA@users.noreply.github.com>
2026-06-24 02:30:27 +0500

    Add Google Tag Manager script to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -160,3 +160,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   )
 }
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-18186688175">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-18186688175');
+</script>
